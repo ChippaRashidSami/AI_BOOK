cbc8a12766b450efadc992136fe843bf557bb28b
ChippaRashidSami <mrashidsami47@gmail.com>
2025-12-26 05:27:38 +0500

    Update docusaurus.config.js

--- book_frontend/docusaurus.config.js
+++ book_frontend/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://hackathon-qecw.vercel.app/', // GitHub Pages URL
+  url: 'https://ai-book-ph2k.vercel.app', // GitHub Pages URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/', // Adjust to your repository name
